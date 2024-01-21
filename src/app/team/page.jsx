bf1a654511b4cd7c741f6342b92ffacd4aab63af
Sonali-Singh31 <sonali.singh.che22@itbhu.ac.in>
2024-01-22 01:54:57 +0530

    dashboard

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -4,22 +4,12 @@ import background from '../image/background.png';
 import '../styles/globals.css';
 const page = () => {
   return (
-    // <div>
-    //   <Image src={background} height={800} width={1000} alt='background image' className={`${styles.featuredImage} w-full`} priority ></Image>
-    //   team
-
-    // </div>
-    // <div className="h-32 w-full " style={{backgroundImage: {background}}}>
-    //   teams
-    // </div>
+    
     <div>
-  {/* // style={{'../image/background.png': `url(${fetchedUrl})`}} 
-  // className='bg-[image:var(../image/background.png)]' */}
+  
     team
     </div>
-    // <div className="h-32 w-full " style="background-image: url('../image/background.png')">
-    //   teams
-    //   </div>
+
   )
 }
 
