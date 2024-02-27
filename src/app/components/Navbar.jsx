b6fa74879c1e139e767c3dd0a01e359b29dea6b0
Pranjal Rajput <96368921+PR4NJ41@users.noreply.github.com>
2024-02-27 14:49:13 +0530

    Revert "Added Events"

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,10 @@
 "use client"
 import React from 'react';
 import Osmoze from '../image/osmoze.png';
-import Image from 'next/image';
 import Link from 'next/link';
+import signIn from '../../../src/app/signIn/page.jsx'
 
-
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
@@ -12,7 +12,7 @@ const Navbar = () => {
     <header className="sticky text-[#8DD5FF]  bg-[#09051B] lg:h-[80px] w-full lg:flex lg:align-middle font-serif shadow-md tracking-wide shadow-black/5 ">
   <div className="container mx-auto flex flex-wrap lg:justify-between  pb-5 flex-col lg:flex-row gap-between  items-center lg:pt-1">
     
-    <ul className="flex lg:w-3/5 lg:justify-start flex-wrap items-center gap-x-4 text-base md:ml-auto lg:gap-x-[4rem]  lg:text-xl">
+    <ul className="flex lg:w-3/5 lg:justify-start flex-wrap items-center gap-x-5 text-base md:ml-auto lg:gap-x-[4rem]  lg:text-xl">
       <li className=" hover:text-[1.26rem]"><Link href="/aboutUs">About Us</Link></li>
       <li className=" hover:text-[1.26rem]"><Link href="/team">Team</Link></li>
       <li className=" hover:text-[1.26rem]"><Link href="/events">Events</Link></li>
