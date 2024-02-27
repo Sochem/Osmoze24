71e6495cbd69fac2c41c5d7b4f6ae036e0989f46
Ritukumawat <ritu.kumawat.che22@itbhu.ac.in>
2024-02-27 15:48:18 +0530

    second commit

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -9,12 +9,12 @@ const page = () => {
       <Navbar/>
 
       {/* Background Image */}
-      <div className="border border-gray-300 rounded-2xl grid-place-items-center p-6 lg:p-0 sm:m-auto w-full">
-        <div className="bg-red-600 bg-[url(/bgImg.png)] bg-cover min-h-screen grid place-items-center">
+      <div className="grid-place-items-center p-6 lg:p-0 sm:m-auto w-full">
+        <div className=" bg-cover min-h-screen grid place-items-center">
 
           {/* Header */}
           <Image
-          className="rounded-full inline-block w-8/12 mt-8 lg:w-2/12"
+          className="inline-block w-8/12 mt-8 lg:w-2/12"
           src="/Events.png"
           width={400}
           height={115}
@@ -31,11 +31,11 @@ const page = () => {
           <div className=" lg:flex lg:gap-12">
 
            {/* first card */}
-          <div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+          <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-                <div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
                 <Image
-                className="rounded-full inline-block "
+                className="inline-block "
                 src="/image.png"
                 width={300}
                 height={300}
@@ -43,10 +43,6 @@ const page = () => {
                 />
             </div>
             <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
-            
-            {/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-              Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-            </div> */}
             </p> 
               <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
             </div>
@@ -55,11 +51,11 @@ const page = () => {
 
 
 
-            <div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+            <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-                <div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
                 <Image
-                className="rounded-full inline-block "
+                className="inline-block "
                 src="/image.png"
                 width={300}
                 height={300}
@@ -67,10 +63,6 @@ const page = () => {
                 />
             </div>
             <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
-            
-            {/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-              Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-            </div> */}
             </p> 
               <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
             </div>
@@ -78,11 +70,11 @@ const page = () => {
 
 
 
-            <div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+            <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-                <div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
                 <Image
-                className="rounded-full inline-block "
+                className="inline-block "
                 src="/image.png"
                 width={300}
                 height={300}
@@ -90,10 +82,6 @@ const page = () => {
                 />
             </div>
             <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
-            
-            {/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-              Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-            </div> */}
             </p> 
               <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
             </div>
@@ -107,72 +95,60 @@ const page = () => {
             <div className="lg:flex lg:gap-12 mb-6">
 
 
-            <div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+            <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-<div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
-<Image
-className="rounded-full inline-block "
-src="/image.png"
-width={300}
-height={300}
-alt='events'
-/>
-</div>
-<p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <Image
+                className="inline-block "
+                src="/image.png"
+                width={300}
+                height={300}
+                alt='events'
+                />
+            </div>
+            <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
+            </p> 
+              <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
+            </div>
 
-{/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-</div> */}
-</p> 
-<div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
-</div>
-
-{/* First Card ends */}
+            {/* First Card ends */}
 
 
 
-<div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+            <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-<div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
-<Image
-className="rounded-full inline-block "
-src="/image.png"
-width={300}
-height={300}
-alt='events'
-/>
-</div>
-<p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
-
-{/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-</div> */}
-</p> 
-<div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
-</div>
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <Image
+                className="inline-block "
+                src="/image.png"
+                width={300}
+                height={300}
+                alt='events'
+                />
+            </div>
+            <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
+            </p> 
+              <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
+            </div>
 
 
 
 
-<div className="border border-gray-50 bg-gray-200/15  mt-9 w-11/12 rounded-2xl flex flex-col lg:w-4/12">
+            <div className="border border-gray-200/15 bg-gray-200/15  mt-9 w-11/12  flex flex-col lg:w-4/12">
 
-<div className='border border-white h-300px w-11/12 m-auto my-4 bg-white rounded-2xl   placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
-<Image
-className="rounded-full inline-block "
-src="/image.png"
-width={300}
-height={300}
-alt='events'
-/>
-</div>
-<p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
-
-{/* <div className='text-white text-lg w-8/12 p-2 o hidden lg:hidden:"false" lg:inline '>
-Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic ipsum ut deserunt repudiandae porro cum ex! Ducimus, aperiam velit?
-</div> */}
-</p> 
-<div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
-</div>
+                <div className=' h-300px w-11/12 m-auto my-4 placeholder-opacity-100 grid place-items-center lg:p-2 lg:m-4 '>
+                <Image
+                className="inline-block "
+                src="/image.png"
+                width={300}
+                height={300}
+                alt='events'
+                />
+            </div>
+            <p className='text-3xl m-auto text-white mb-2 ' >OsmoCross 
+            </p> 
+              <div className='flex flex-row-reverse mr-2'><a href='#' className='font-thin mb-2 w-3/12 text-center text-black border rounded-md border-white bg-white'>Read more </a></div>
+            </div>
 
 
           
