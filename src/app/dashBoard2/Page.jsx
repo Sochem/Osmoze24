17f4472ed5d0ec1b974585caf911df41fe5f0666
Sonali-Singh31 <sonali.singh.che22@itbhu.ac.in>
2024-02-27 16:06:42 +0530

    changed

--- src/app/dashBoard2/Page.jsx
+++ src/app/dashBoard2/Page.jsx
@@ -15,13 +15,13 @@ const page = () => {
     <>
     
     <section className="sticky text-[#8DD5FF]  bg-[#07000B] lg:h-[100vh] lg:flex lg:flex:col shadow-md tracking-wide shadow-black/5 w-1/5">
-  <div className="container mx-auto flex gap-y-5 flex-wrap pb-5 flex-col lg:flex-col">
-    <ul className="flex  lg:flex-col flex-wrap gap-y-6  text-base  lg:text-[1.2rem] text-center">
-      <li className="hover:underline hover:text-[1.23rem]"><Link href="/introduction">Introduction</Link></li>
-      <li className="hover:underline hover:text-[1.23rem]"><Link href="/dashBoard">Dashboard</Link></li>
-      <li className="hover:underline hover:text-[1.23rem]"><Link href="/eventRegistration">Events Registration</Link></li>
-      <li className="hover:underline hover:text-[1.23rem]"><Link href="/contactUs">Contact Us</Link></li>
-      <li className="hover:underline hover:text-[1.23rem]"><Link href="profile">Profile</Link></li>
+  <div className="container mx-auto flex lg:gap-y-10 flex-wrap pb-5 flex-col lg:flex-col">
+    <ul className="flex  lg:flex-col flex-wrap lg:gap-y-8  text-base  lg:text-[1.4rem] text-center">
+     
+      <li className="hover:underline hover:text-[1.43rem]"><Link href="/dashBoard">Dashboard</Link></li>
+      <li className="hover:underline hover:text-[1.43rem]"><Link href="/eventRegistration">Events Registration</Link></li>
+      <li className="hover:underline hover:text-[1.43rem]"><Link href="/contactUs">Contact Us</Link></li>
+      <li className="hover:underline hover:text-[1.43rem]"><Link href="profile">Profile</Link></li>
     </ul>
     <div className=" order-first    lg:flex m-0 p-2 ">
     <Link href="/">
