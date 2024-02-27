2f52b09d20fdfb5997697765ebcc00414f9725eb
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2024-02-27 15:26:24 +0530

    Fixed Background [PR4NJ41]

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,12 +19,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <AuthContextProvider>
         <body className="flex flex-col min-h-screen">
-          {/* <Navbar /> */}
           <div className="background-container ">
             <BackgroundEffect />
-            <div className="children-container ">{children}</div>
           </div>
-          {/* <Footer /> */}
+          <div className="children-container ">{children}</div>
           <Script src="script.js"></Script>
         </body>
 
